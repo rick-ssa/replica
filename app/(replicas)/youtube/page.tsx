051bef59ac7bde97edec1420262a08e36788baa5
rick-ssa <rick_ssa@hotmail.com>
2024-07-29 22:59:34 -0300

    rollback to use router handlers

--- app/(replicas)/youtube/page.tsx
+++ app/(replicas)/youtube/page.tsx
@@ -1,11 +1,7 @@
 import { GroupMenu } from "./_components/groupMenu/groupMenu"
 
 export default async function YoutubePage() {
-    const response = await (
-        await fetch("https://randomuser.me/api/?results=10", {
-            next: { revalidate: 60 * 60 * 24 },
-        })
-    ).json()
+    const response = await (await fetch(`${process.env.URL}/api/users`)).json()
     const users = response.results
     return <GroupMenu users={users} />
 }
