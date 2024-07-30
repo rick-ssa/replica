a8a4068006e45932c9504500f5ac208366de07c2
rick-ssa <rick_ssa@hotmail.com>
2024-07-29 23:27:25 -0300

    revalidate users each 10 min

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
     const users = await (
         await fetch("https://randomuser.me/api/?results=10", {
-            next: { revalidate: 60 * 60 * 24 },
+            next: { revalidate: 60 * 10 },
         })
     ).json()
 
