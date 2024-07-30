e626eab04ce399d5e14c4dbc179ee49ec4089d0e
rick-ssa <rick_ssa@hotmail.com>
2024-07-29 22:56:26 -0300

    making call directly to random.me

--- app/(replicas)/youtube/page.tsx
+++ app/(replicas)/youtube/page.tsx
@@ -1,7 +1,11 @@
 import { GroupMenu } from "./_components/groupMenu/groupMenu"
 
 export default async function YoutubePage() {
-    const response = await (await fetch(`${process.env.URL}/api/users`)).json()
+    const response = await (
+        await fetch("https://randomuser.me/api/?results=10", {
+            next: { revalidate: 60 * 60 * 24 },
+        })
+    ).json()
     const users = response.results
     return <GroupMenu users={users} />
 }
