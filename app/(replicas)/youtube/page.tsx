04640faf3168c4825e8ee88172620ff1d70bf7cf
rick-ssa <rick_ssa@hotmail.com>
2024-07-29 22:53:26 -0300

    little change

--- app/(replicas)/youtube/page.tsx
+++ app/(replicas)/youtube/page.tsx
@@ -1,7 +1,7 @@
 import { GroupMenu } from "./_components/groupMenu/groupMenu"
 
 export default async function YoutubePage() {
-    const response = await (await fetch(`${process.env.URL}/api/users/`)).json()
+    const response = await (await fetch(`${process.env.URL}/api/users`)).json()
     const users = response.results
     return <GroupMenu users={users} />
 }
