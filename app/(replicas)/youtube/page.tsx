1631e6aa1f8ff892d83bc6431a55143d8146d56e
rick-ssa <rick_ssa@hotmail.com>
2024-07-29 22:27:17 -0300

    added dot env

--- app/(replicas)/youtube/page.tsx
+++ app/(replicas)/youtube/page.tsx
@@ -1,9 +1,7 @@
 import { GroupMenu } from "./_components/groupMenu/groupMenu"
 
 export default async function YoutubePage() {
-    const response = await (
-        await fetch("http://localhost:3000/api/users/")
-    ).json()
+    const response = await (await fetch(`${process.env.URL}/api/users/`)).json()
     const users = response.results
     return <GroupMenu users={users} />
 }
